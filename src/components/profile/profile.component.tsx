8e0dc3b3250f53b56b745c1fd68a548e76bc36a6
Bastien Faisant <bastien.faisant@etudiant.univ-rennes.fr>
2023-10-22 17:32:29 +0200

    add linked, gmail and resume links

--- src/components/profile/profile.component.tsx
+++ src/components/profile/profile.component.tsx
@@ -1,13 +1,34 @@
+import { useTranslation } from 'react-i18next';
+import { useEffect, useState } from 'react';
+
 import { usePortfolio } from '../../modules/portfolio';
+import CV_FR from '../../../public/cv/Bastien_Faisant_CV.pdf';
+import CV_EN from '../../../public/cv/Bastien_Faisant_english_CV.pdf';
+import { LanguagesEnum } from '../../enums/languages.enum';
 
 interface ProfileProps {
   categoriesRef: React.MutableRefObject<HTMLElement[]>
 }
 
 export const Profile = ({ categoriesRef }: ProfileProps) => {
+  const { t, i18n } = useTranslation();
+
+  const [cvPdf, setCVPdf] = useState(CV_EN);
+
   const { findProfile } = usePortfolio();
 
   const profile = findProfile();
+
+  useEffect(() => {
+    switch (i18n.language) {
+      case LanguagesEnum.FR:
+        setCVPdf(CV_FR);
+        break;
+      default:
+        setCVPdf(CV_EN);
+        break;
+    }
+  }, [i18n.language]);
 
   return (
     <section
@@ -20,6 +41,23 @@ export const Profile = ({ categoriesRef }: ProfileProps) => {
         <h2 className="text-lg sm:text-3xl md:text-3xl lg:text-4xl text-center text-primary-200">{profile.status}</h2>
       </div>
       <p>{profile.description}</p>
+      <div className="flex flex-row justify-center items-center gap-8">
+        <a href="https://www.linkedin.com/in/bastienfaisant/" target="_blank" rel="noreferrer">
+          <img src="linkedin.png" alt="LinkedIn" className="h-8 w-8" />
+        </a>
+        <a href="mailto:fst09bastien@gmail.com" target="_blank" rel="noreferrer">
+          <img src="gmail.svg" alt="Gmail" className="h-8 w-8" />
+        </a>
+        <a
+          href={cvPdf}
+          // download={`cv_${i18n.language}_bastien_faisant`} // Directly download the PDF file
+          // target="_blank" // Open the PDF file in another tab
+          className="h-full flex justify-center items-center p-[10px] rounded-2xl bg-background-200 dark:bg-background-700 text-text-700 dark:text-text-200"
+          rel="noreferrer"
+        >
+          {t('downloadResume')}
+        </a>
+      </div>
     </section>
   );
 };
